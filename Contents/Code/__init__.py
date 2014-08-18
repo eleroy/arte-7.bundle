dea83b2e4c022f931e878f2260f37c66a47dea0c
eleroy <edoleroy@gmail.com>
2014-08-18 15:11:52 +0200

    Rework of the plugins to get the streams right.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,10 +1,6 @@
 VIDEO_PREFIX = "/video/artep7"
-#MUSIC_PREFIX = "/music/artep7"
 
-VIDEOS_PAGE = 'http://videos.arte.tv/%s/videos'
-BASE_ADDRESS = 'http://videos.arte.tv'
-SUBCATEGORY = 'http://videos.arte.tv%s,view,rss.xml'
-VIDEO_PAGE = 'http://videos.arte.tv/%s/do_delegate/videos/%s,view,asPlayerXml.xml'
+VIDEOS_PAGE = 'http://www.arte.tv/guide/%s/'
 
 NAME = 'Arte+7'
 
@@ -18,89 +14,88 @@ PREF_STRING = dict({"en":"Preferences","de":"Einstellungen","fr":"Préférences"
 
 def Start():
 
-    Plugin.AddPrefixHandler(VIDEO_PREFIX, VideoMainMenu, NAME, ICON, ART)
+	Plugin.AddPrefixHandler(VIDEO_PREFIX, VideoMainMenu, NAME, ICON, ART)
 
-    Plugin.AddViewGroup("InfoList", viewMode="InfoList", mediaType="items")
-    Plugin.AddViewGroup("List", viewMode="List", mediaType="items")
+	Plugin.AddViewGroup("InfoList", viewMode="InfoList", mediaType="items")
+	Plugin.AddViewGroup("List", viewMode="List", mediaType="items")
 
-    MediaContainer.art = R(ART)
-    MediaContainer.title1 = NAME
-    DirectoryItem.thumb = R(ICON)
+	MediaContainer.art = R(ART)
+	MediaContainer.title1 = NAME
+	DirectoryItem.thumb = R(ICON)
 
-    HTTP.CacheTime = 3600
+	HTTP.CacheTime = 3600
 
 def VideoMainMenu():
-    dir = MediaContainer(viewGroup="List",noCache=True)
-    mainpage = HTML.ElementFromURL(VIDEOS_PAGE % Prefs['lang'])
-    for category in mainpage.xpath('//ul[@id="nav"]/li[not(@class="selected") and not(@class="lastItem")]/a'):  
-      dir.Append(Function(DirectoryItem(CategoryParsing,category.text,thumb=R(ICON),art=R(ART)),path = category.get('href')))
-
-    dir.Append(PrefsItem(title=PREF_STRING[Prefs['lang']],subtile="",summary="",thumb=R(PREFS)))
-
-    return dir
+	dir = MediaContainer(viewGroup="List",noCache=True)
+	mainpage = HTML.ElementFromURL(VIDEOS_PAGE%(Prefs['lang'],)+"plus7")
+	for category in mainpage.xpath('//div[@class="choice-by_channel row-fluid choices visible-phone"]/ul[@data-filter="by_channel"]/li[@class="span2"]/a'):
+		Log(category.text)
+		dir.Append(Function(DirectoryItem(CategoryParsing,category.text,thumb=R(ICON),art=R(ART)),path = category.text.strip()))
+	dir.Append(PrefsItem(title=PREF_STRING[Prefs['lang']],subtile="",summary="",thumb=R(PREFS)))
+	return dir
 
 def CategoryParsing(sender,path):
-    dir = MediaContainer(viewGroup="List")
-    pagetoscrape = HTML.ElementFromURL(BASE_ADDRESS + path)
-    for category in pagetoscrape.xpath("//div[@id='listChannel']/ul//a"):
-      link = category.get('href')
-      if '.html' in link:
-        path = link
-      else:
-        path = link + '/id-' +category.xpath('span')[0].get('id')
-      Log(path)
-      dir.Append(Function(DirectoryItem(SubCategoryParsing,category.text,thumb=R(ICON),art=R(ART)),path = path))
-    return dir
+	dir = ObjectContainer(view_group='InfoList',title2 = path, art = R(ART))
+	catscrape = JSON.ObjectFromURL(VIDEOS_PAGE%(Prefs['lang'],)+"plus7.json")
+	for category in catscrape['videos']:
+		if path in category['video_channels'].split(', '):
+			sum=category['airdate_long'].capitalize() + '.\n' + category['desc'] + 'Categories : ' + category['video_channels'] + '. Durée : ' + str(category['duration']) + ' min.'
+			if(Prefs['lang']=="de"):
+				vid = JSON.ObjectFromURL("http://arte.tv/papi/tvguide/videos/stream/player/D/" + category['em'] +"_PLUS7-D/ALL/ALL.json")
+			else:
+				vid = JSON.ObjectFromURL("http://arte.tv/papi/tvguide/videos/stream/player/F/" + category['em'] +"_PLUS7-F/ALL/ALL.json")
+			rating_key = '%s%s'
+			dir.add(
+				VideoClipObject(
+					key = Callback(Lookup, title=vid["videoJsonPlayer"]["VTI"], rating_key=rating_key, url=vid["videoJsonPlayer"]["VSR"]["HTTP_MP4_SQ_1"]["url"], url2=vid["videoJsonPlayer"]["VSR"]["HTTP_MP4_MQ_1"]["url"], thum=category['image_url'], sum=sum),
+					url = vid["videoJsonPlayer"]["VSR"]["HTTP_MP4_SQ_1"]["url"],
+					title = vid["videoJsonPlayer"]["VTI"],
+					thumb = Resource.ContentsOfURLWithFallback(url=category['image_url'], fallback="icon-default.png"),
+					summary = sum,
+					items = [
+						MediaObject(
+						video_resolution = 720,
+						video_codec = "H264",
+						container = "mp4",
+						parts = [PartObject(key=Callback(PlayVideo, url=vid["videoJsonPlayer"]["VSR"]["HTTP_MP4_SQ_1"]["url"]))]                
+						),MediaObject(
+						video_resolution = 480,
+						video_codec = "H264",
+						container = "mp4",
+						parts = [PartObject(key=Callback(PlayVideo, url=vid["videoJsonPlayer"]["VSR"]["HTTP_MP4_MQ_1"]["url"]))]                
+						)
+					]
+				)
+			)
 
+	return dir
 
-def SubCategoryParsing(sender,path):
-    dir = MediaContainer(viewGroup="List")
-    path = path.replace('/videos','/do_delegate/videos').replace('.html','')
-    rss = XML.ElementFromURL(SUBCATEGORY  % path)
-    for item in rss.xpath('//item'):
-      title = item.xpath("title")[0].text
-      summary = item.xpath("description")[0].text
-      link = item.xpath("link")[0].text
-      videoid = link[link.rfind("/")+1:link.find(".html")]
-      dir.Append(Function(DirectoryItem(GetAllVideos,title = title,summary = summary,thumb=R(ICON),art=R(ART)),title = title,summary = summary,videoid = videoid))
-    return dir
+def PlayVideo(url):
+	 return Redirect(url)
 
-def GetAllVideos(sender,title,summary,videoid):
-    dir = MediaContainer(viewGroup="InfoList")
-    xml = XML.ElementFromURL(VIDEO_PAGE  % (Prefs['lang'],videoid))
-    for item in xml.xpath('//videos/video'):
-      if item.get('lang') == Prefs['lang']:
-        localtitle = title# + ' - ' + item.get('lang')
-        link = item.get("ref")
-# TO WORK WITH DIRECT LINKS REMOVE THIS SECTION   
-        xml = XML.ElementFromURL(link)
-        thumb = xml.xpath('//firstThumbnailUrl')[0].text
-        path = xml.xpath('//video/url')[0].text
-        summary = HTML.ElementFromURL(path).xpath("//div[@class='recentTracksCont']/div/p")[0].text
-        subtitle = xml.xpath("//dateVideo")[0].text
-        subtitle = subtitle[:subtitle.find('+')-1]
-        dir.Append(Function(VideoItem(PlayVideo, title = localtitle, summary = summary, subtitle = subtitle, thumb=thumb),path=path))
-#AND USE THIS      
-#      dir.Append(Function(DirectoryItem(GetVideos,title = localtitle,summary = summary,thumb=R(ICON),art=R(ART)),title = localtitle,summary = summary,path = link))
-    if len(dir) == 0:
-      dir = MessageContainer("Error","No video available in this language")
-    return dir    
-
-# TO WORK WITH DIRECT LINKS UNCOMMENT THIS SECTION TOEXPOSE A SUBMENU FOR DIFFERENT QUALITY STREAMS
-#def GetVideos(sender,title,summary,path):
-#    dir = MediaContainer(viewGroup="List")
-#    xml = XML.ElementFromURL(path)
-#    thumb = xml.xpath('//firstThumbnailUrl')[0].text
-#    localtitle = title
-#    link = xml.xpath('//video/url')[0].text
-#    summary = HTML.ElementFromURL(link).xpath("//div[@class='recentTracksCont']/div/p")[0].text
-#    subtitle = xml.xpath("//dateVideo")[0].text
-#    dir.Append(Function(VideoItem(PlayVideo, title = localtitle, summary = summary, subtitle = subtitle, thumb=thumb),path=link))
-    #for item in xml.xpath('//urls/url'):
-    #  localtitle = title + ' - ' + item.get('quality')
-    #  link = item.text#.split("MP4:")
-    #  dir.Append(RTMPVideoItem(link, clip = '',width = 640,height = 480, title = localtitle,summary = summary,thumb=thumb))
-#    return dir
-
-def PlayVideo(sender, path):
-	return Redirect(WebVideoItem(path)) 
+def Lookup(title, rating_key, url, url2, thum, sum):
+	oc = ObjectContainer()
+	oc.add(
+		VideoClipObject(
+			key = Callback(Lookup, title=title, rating_key=rating_key, url=url, url2=url2, thum=thum, sum=sum),
+			title = title,
+			rating_key = rating_key,
+			thumb = Resource.ContentsOfURLWithFallback(url=thum, fallback="icon-default.png"),
+			summary = sum,
+			items = [
+				MediaObject(
+					video_resolution = 720,
+					video_codec = "H264",
+					container = "mp4",
+					parts = [PartObject(key=Callback(PlayVideo, url=url))]
+				),
+				MediaObject(
+					video_resolution = 480,
+					video_codec = "H264",
+					container = "mp4",
+					parts = [PartObject(key=Callback(PlayVideo, url=url2))]
+				)
+			]
+		)
+	)
+	return oc
